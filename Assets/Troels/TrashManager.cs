753b20768efac67b97ff8d8bf0a7f790e2c81dec
JohanPedersen11 <Johan.munch.2002@gmail.com>
2025-02-27 14:56:01 +0100

    Squashed commit of the following:
    commit 93dd46f3c3f74a1fad9c61f95816f35c0f585990
    Author: LameIkon <troelsjelsbak@hotmail.com>
    Date:   Wed Feb 26 23:54:35 2025 +0100
    
        Remebering to Save the Scenes
    
    commit fccf1b90c8507463e1fa732bf781b7dcf09b5212
    Author: LameIkon <troelsjelsbak@hotmail.com>
    Date:   Wed Feb 26 23:53:34 2025 +0100
    
        Trash System

---
+++ Assets/Troels/TrashManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class TrashManager : MonoBehaviour
+{
+	//TODO: This GameObject should be have a Singleton Pattern but I cannot remember how to implement it in Unity right now.
+
+
+	//TODO: This is just for debugging it should be displayed elseware.
+	public float _Points;
+
+
+	public void OnEnable() 
+	{
+		TrashBin.OnTrashedEvent += HandleTrashEvent; // Adds the HandleTrashEvent to the Action OnTrashedEvent
+	}
+
+	public void OnDisable()
+	{
+		TrashBin.OnTrashedEvent -= HandleTrashEvent;
+	}
+
+
+	private void HandleTrashEvent(float points) 
+	{
+		_Points += points;
+	}
+
+}
