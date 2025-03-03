8f4684322b90e8bb878d3ff5b4f43c21a2a3911e
4sela <adawa21@student.sdu.dk>
2025-03-04 00:59:40 +0100

    More

--- Assets/_Scripts/HighscoreTable.cs
+++ Assets/_Scripts/HighscoreTable.cs
@@ -1,5 +1,8 @@
+using System;
+using JetBrains.Annotations;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class HighscoreTable : MonoBehaviour
 {
@@ -8,7 +11,8 @@ public class HighscoreTable : MonoBehaviour
     // By using the Singleton pattern, we can make the UpdateHighScorePoints() method static and call it 
     // only when the score should be changed, improving performance.
 
-    [SerializeField] private TextMeshProUGUI _pointCounter;
+    [SerializeField] private TextMeshProUGUI _totalScore;
+    [SerializeField] private TextMeshProUGUI _scoreIncremenTracker;
     private static           HighscoreTable  _instance;
 
     private void Awake()
@@ -19,17 +23,36 @@ public class HighscoreTable : MonoBehaviour
 
     private void Start()
     {
-        _pointCounter.text = 0.ToString();
+        if (_totalScore is not null)
+        {
+            _totalScore.text = 0.ToString();
+        }
+
+        if (_scoreIncremenTracker is not null)
+        {
+            _scoreIncremenTracker.text = "";
+        }
     }
 
     public static void UpdateHighScorePoints(float? points)
     {
         if (_instance is null) return;
 
-        float  currentPoints     = float.Parse(_instance._pointCounter.text);
+        float  currentPoints     = float.Parse(_instance._totalScore.text);
         float? incrementedPoints = currentPoints;
 
-        incrementedPoints            += points;
-        _instance._pointCounter.text =  incrementedPoints.ToString();
+        incrementedPoints                    += points;
+        _instance._totalScore.text           =  incrementedPoints.ToString();
+        _instance._scoreIncremenTracker.text =  FormatScoreIncrementTracker();
+        return;
+        
+        String FormatScoreIncrementTracker()
+        {
+            return points switch
+            {
+                
+            };
+        }
     }
+    
 }
