f10cb0a68b2c6912d0e8481048e9db82cb62632a
JohanPedersen11 <Johan.munch.2002@gmail.com>
2025-03-06 12:48:58 +0100

    Squashed commit of the following:
    commit 9b79538e1ccb4d4c8300ba61fd26655b2f30cd28
    Author: JohanPedersen11 <Johan.munch.2002@gmail.com>
    Date:   Thu Mar 6 12:48:29 2025 +0100
    
        merged complete
    
    commit e869987a7874248310a1303147b342e9240349a9
    Merge: 43c4c69 85900f1
    Author: JohanPedersen11 <Johan.munch.2002@gmail.com>
    Date:   Thu Mar 6 12:35:04 2025 +0100
    
        Merge branch 'AdamDev4' into MergeBranch
    
    commit 85900f10fa00aa0207b80a9f4d1f40f012f89780
    Author: 4sela <adawa21@student.sdu.dk>
    Date:   Thu Mar 6 12:34:49 2025 +0100
    
        updated prefab
    
    commit 43c4c692b7aa162775b4ffdfa386b0f8908bd913
    Merge: 2b27b09 5e1ecb4
    Author: JohanPedersen11 <Johan.munch.2002@gmail.com>
    Date:   Thu Mar 6 12:23:29 2025 +0100
    
        Merge remote-tracking branch 'origin/JonasDev-3' into MergeBranch
    
    commit 2b27b09ecafcfc22b6d34f3350c1cebf4b12fbdc
    Merge: 3946013 b13ab17
    Author: JohanPedersen11 <Johan.munch.2002@gmail.com>
    Date:   Thu Mar 6 12:23:06 2025 +0100
    
        Merge remote-tracking branch 'origin/TroelsDev' into MergeBranch
    
    commit 39460139a8596abb9f65f69f9f4d253ef224300e
    Merge: 2dd2403 d50c292
    Author: JohanPedersen11 <Johan.munch.2002@gmail.com>
    Date:   Thu Mar 6 12:22:39 2025 +0100
    
        Merge branch 'AdamDev4' into MergeBranch
    
    commit d50c292f8a0d5424ea95e8098ea0dbb0f930d340
    Author: 4sela <adawa21@student.sdu.dk>
    Date:   Thu Mar 6 12:19:23 2025 +0100
    
        Error Message Warning
    
    commit 5e1ecb4335eb66b2c3091ab56e74d1a297713333
    Author: chaplinchap <jonas.linnemann@live.dk>
    Date:   Wed Mar 5 19:12:06 2025 +0100
    
        TrashUI text from SO data
    
    commit 506835fc3407d0dd966863a33dbb086867098047
    Author: 4sela <adawa21@student.sdu.dk>
    Date:   Wed Mar 5 14:19:48 2025 +0100
    
        small
    
    commit 2dd2403d9a26b93db8744e59628e53b3a5cfddcb
    Author: TootSnoot <victoriakippa@gmail.com>
    Date:   Wed Mar 5 12:23:10 2025 +0100
    
        New scene + text PreFab
    
        Just setting things up.
    
    commit b13ab17be8e0f1a2ed91d3491cb276e4ae888fe6
    Merge: c086bce 93dd46f
    Author: LameIkon <troelsjelsbak@hotmail.com>
    Date:   Wed Mar 5 11:37:57 2025 +0100
    
        Merge remote-tracking branch 'origin/TroelsDev' into TroelsDev
    
    commit c086bce6c479ad65b9656524ba06456c3258486a
    Author: LameIkon <troelsjelsbak@hotmail.com>
    Date:   Wed Feb 26 23:54:35 2025 +0100
    
        Remebering to Save the Scenes
    
    commit c8d9d513fd072fa1ad43d7f9ff51fbbe85cdb996
    Author: LameIkon <troelsjelsbak@hotmail.com>
    Date:   Wed Feb 26 23:53:34 2025 +0100
    
        Trash System
    
    commit 93dd46f3c3f74a1fad9c61f95816f35c0f585990
    Author: LameIkon <troelsjelsbak@hotmail.com>
    Date:   Wed Feb 26 23:54:35 2025 +0100
    
        Remebering to Save the Scenes
    
    commit fccf1b90c8507463e1fa732bf781b7dcf09b5212
    Author: LameIkon <troelsjelsbak@hotmail.com>
    Date:   Wed Feb 26 23:53:34 2025 +0100
    
        Trash System

--- Assets/_Scripts/Trash/TrashDescriptionUIEnable.cs
+++ Assets/_Scripts/Trash/TrashDescriptionUIEnable.cs
@@ -1,13 +1,27 @@
 using System.Buffers.Text;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrashDescriptionUIEnable : MonoBehaviour
 {
     [SerializeField] private GameObject _trashUIPanel;
     private Transform _playerCamera; // NOT XR RIG - MAIN CAMERA OR GAZE INTERACTOR (CHILD OF XR RIG)
 
+    [SerializeField] private SOTrashData _trashData;
+    private TextMeshProUGUI _headerText;
+    private TextMeshProUGUI _contentText;
+
+
+    private void Awake()
+    {
+        _headerText = _trashUIPanel.GetComponentsInChildren<TextMeshProUGUI>()[0]; //first found, should be Header
+        _contentText = _trashUIPanel.GetComponentsInChildren<TextMeshProUGUI>()[1]; 
+
+        UpdateUI(); //we update the texts in awake before Start is called. 
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +31,7 @@ public class TrashDescriptionUIEnable : MonoBehaviour
         if (_playerCamera == null)
         {
             _playerCamera = Camera.main.transform; // Auto-assign VR camera
-        }
+        }    
     }
 
     // Update is called once per frame
@@ -28,9 +42,9 @@ public class TrashDescriptionUIEnable : MonoBehaviour
 
     public void OnGrab()
     {
-        _trashUIPanel.SetActive(true);
+        _trashUIPanel.SetActive(true);     
         _trashUIPanel.transform.SetParent(_playerCamera);
-        _trashUIPanel.transform.localScale = new Vector3(0.5f,0.5f,0.5f); 
+        _trashUIPanel.transform.localScale = new Vector3(0.5f,0.5f,0.5f);     
     }
 
     public void OnRelease()
@@ -39,5 +53,15 @@ public class TrashDescriptionUIEnable : MonoBehaviour
         _trashUIPanel.transform.SetParent(null);
     }
 
-   
+    private void UpdateUI()
+    {
+        if (_headerText != null)
+        {
+            _headerText.text = _trashData._Name; 
+        }
+        if (_contentText != null)
+        {
+            _contentText.text = _trashData._Description;
+        }
+    }
 }
