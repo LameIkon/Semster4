fd3c0353fc2a1db1cdca50fc5fa8f5315630645b
LameIkon <troelsjelsbak@hotmail.com>
2025-03-27 14:25:12 +0100

    Main updated

--- Assets/_Scripts/Trash/ITrashable.cs
+++ Assets/_Scripts/Trash/ITrashable.cs
@@ -1,8 +1,10 @@
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 using UnityEngine;
 public interface ITrashable
 {
-	float Trashing(SortingCategory type);
-	SOTrashData TrashData();
+    float Trashing(SortingCategory type);
+    SOTrashData TrashData();
 	AudioClip TrashingSound();
 	void PickUpSound();
 }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
