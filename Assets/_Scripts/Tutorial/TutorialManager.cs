8fc18c23319808e243d2aa94ebdd748175099633
JohanPedersen11 <Johan.munch.2002@gmail.com>
2025-03-30 22:41:49 +0200

    save before changing stuff that might ruin stuff

--- Assets/_Scripts/Tutorial/TutorialManager.cs
+++ Assets/_Scripts/Tutorial/TutorialManager.cs
@@ -9,33 +9,48 @@ public class TutorialManager : Singleton<TutorialManager>
     [Header("Main Components")]
     [SerializeField] private SOTutorialData[] SO_tutorialData;
     [SerializeField] private GameObject[] _highlightDots;
-    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private TextMeshProUGUI _Descriptiontext;
     [SerializeField] private GameObject _continueButton;
+    private PlayerVR _playerVR;
     private int _index;
 
-    // Second tutorial parts
-    [Header("Second Objective")]
+    [Header("Objectives")]
     [SerializeField] private SOTutorialData[] SO_tutorialObjectiveTwo;
-    private int _totalTrashAmountThrownOut = 3; // Counted as 4, since index 0 counts also
+    [SerializeField] private TextMeshProUGUI _ObjectivetextOne;
+    [SerializeField] private TextMeshProUGUI _Objectivetexttwo;
+
+
+    private const int _totalTrashAmountThrownOut = 3; // Counted as 4, since index 0 counts also
     private int _currentTrashThrownOut;
+
+    private const int _totalInspectedAmount = 3; // Counted as 4, since index 0 counts also
+    private int _currentInspectedObjects;
 
     public void OnEnable()
     {
-        TrashBin.s_OnTrashedEvent += ChecksortingTrashAmount; // Adds the HandleTrashEvent to the Action OnTrashedEvent
+        TrashBin.s_OnTrashedEvent += SecondObjective; // Adds the HandleTrashEvent to the Action OnTrashedEvent
+        TrashBin.s_OnTrashedEvent += ThirdObjectiveTrashObjects;
     }
 
     public void OnDisable()
     {
-        TrashBin.s_OnTrashedEvent -= ChecksortingTrashAmount;
+        TrashBin.s_OnTrashedEvent -= SecondObjective;
+        TrashBin.s_OnTrashedEvent -= ThirdObjectiveTrashObjects;
     }
 
     private void Start()
     {
+        _playerVR = PlayerVR.S_Instance;
         SetTutorialQuest(_index);
     }
 
 
     public void OnContinueButtonPressed() // Button
+    {
+        ContinueTutorial();
+    }
+
+    private void ContinueTutorial()
     {
         _index++;
 
@@ -51,22 +66,9 @@ public class TutorialManager : Singleton<TutorialManager>
 
     private void SetTutorialQuest(int index)
     {
-        _text.text = SO_tutorialData[index].SO_Description;
+        _Descriptiontext.text = SO_tutorialData[index].SO_Description;
         _highlightDots[index-1].SetActive(false); // Deactive previous dot
         _highlightDots[index].SetActive(true); // Activate next dot.
-
-        //switch (index)  // Assign quest
-        //{
-        //    case 0:
-        //        CheckHoldingObject();
-        //        break;
-        //    case 1:
-        //        //_currentTutorialIndex = CheckHoldingAndInspecting;
-        //        break;
-        //    default:
-        //        //_currentTutorialIndex = null;
-        //        break;
-        //}
     }
 
     private void Update()
@@ -79,7 +81,7 @@ public class TutorialManager : Singleton<TutorialManager>
                 break;
             case 1: // Complete Objective to continue
                 _continueButton.SetActive(false);
-                CheckHoldingObject(); 
+                FirstObjective(); 
                 break;
             case 2: // Press continue button
                 _continueButton.SetActive(true);
@@ -96,32 +98,72 @@ public class TutorialManager : Singleton<TutorialManager>
     }
 
 
-    private void CheckHoldingObject()
+    private void FirstObjective() // Hold Object
     {
-        if (PlayerVR.S_Instance.IsHoldingObject())
+        if (_playerVR.IsHoldingObject())
         {
-            OnContinueButtonPressed();
+            ContinueTutorial();
         }
     }
 
-    private void ChecksortingTrashAmount(GameObject sender, float points)
+    private void SecondObjective(GameObject sender, float points) // Trash object
     {
         Debug.Log(_currentTrashThrownOut);
         if (_currentTrashThrownOut == _totalTrashAmountThrownOut)
         {
-            OnContinueButtonPressed();
+            ContinueTutorial();
+            _currentTrashThrownOut = 0; // Resetted for other use cases
             return;
         }
         else
         {
-            _text.text = SO_tutorialObjectiveTwo[_currentTrashThrownOut].SO_Description;
+            _Descriptiontext.text = SO_tutorialObjectiveTwo[_currentTrashThrownOut].SO_Description;
             _currentTrashThrownOut++;
         }
 
     }
 
-    private void CheckHoldingAndInspecting()
+    private void ThirdObjectiveInspectObjects()
     {
-        //if (PlayerVR.S_Instance.Is)
+        if (_playerVR._IsInspectingObjectButton && _playerVR.IsHoldingObject()) // Inspect
+        {
+            if (_currentInspectedObjects < _totalInspectedAmount) // Yellow text
+            {
+                _ObjectivetextOne.text = ($"<b><color=#FFFF00>{_currentInspectedObjects}</color>/{_totalInspectedAmount} inspiceret affald</b>");
+            }
+            else // Green text
+            {
+                _ObjectivetextOne.text = ($"<b><color=#00FF00>{_currentInspectedObjects}</color>/{_totalInspectedAmount} inspiceret affald</b>");
+            }
+            _currentInspectedObjects++;
+        }
+
+        if (_currentTrashThrownOut >= _totalTrashAmountThrownOut && _currentInspectedObjects >= _totalInspectedAmount) // When both inspection and trashing is complete
+        {
+            ContinueTutorial();
+        }
+    }
+
+    private void ThirdObjectiveTrashObjects(GameObject sender, float points) //trash object
+    {
+        if (_currentTrashThrownOut < _totalTrashAmountThrownOut) // Yellow Text
+        {
+            _Objectivetexttwo.text = ($"<b><color=#FFFF00>{_currentInspectedObjects}</color>/{_totalInspectedAmount} affald sorteret</b>");
+        }
+        else // Green Text
+        {
+            _ObjectivetextOne.text = ($"<b><color=#00FF00>{_currentInspectedObjects}</color>/{_totalInspectedAmount} affald sorteret</b>");
+        }
+        _currentTrashThrownOut++;
+
+        if (_currentTrashThrownOut >= _totalTrashAmountThrownOut && _currentInspectedObjects >= _totalInspectedAmount) // When both inspection and trashing is complete
+        {
+            ContinueTutorial();
+        }
+    }
+
+    private void FourthObjective() // Open door
+    {
+
     }
 }
