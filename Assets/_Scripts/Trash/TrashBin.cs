29fbaa1c312ee5d9d5b6612056040003ec8c2c53
4sela <adawa21@student.sdu.dk>
2025-03-30 17:43:00 +0200

    Took a look at the TrashBin
    Wanted to see how it the script was set up

--- Assets/_Scripts/Trash/TrashBin.cs
+++ Assets/_Scripts/Trash/TrashBin.cs
@@ -4,105 +4,100 @@ using System;
 using UnityEngine;
 using TMPro;
 
+/// <summary>
+/// The implementation of the TrashBin handles the sending of data to the TrashManager,
+/// It is also responsible for calling the Trashing method on the ITrashables
+/// </summary>
 [RequireComponent(typeof(BoxCollider), typeof(Animator), typeof(AudioSource)),
-	RequireComponent(typeof(Rigidbody))]
-public class TrashBin : MonoBehaviour, ILogable
+ RequireComponent(typeof(Rigidbody))]
+public class TrashBin : MonoBehaviour, ILoggable
 {
-	/* The implementation of the TrashBin handles the sending of data to the TrashManager,
-	 * It is also responsible for calling the Trashing method on the ITrashables
-	 */
+    // Data
+    [SerializeField]
+    private SOTrashBinData _binData; // The Trash type comes from the SO 
 
-	[SerializeField]
-	private SOTrashBinData _binData; // The Trash type comes from the SO 
+    // Events
+    public static event Action<GameObject, float> s_OnTrashedEvent; // The event that connects to the TrashManager
+    public static event Action<float, SOTrashData> s_OnTrashedEvent2;
+    public static event Action<string, string, float> s_OnLogEvent;
+    
+    // Animations
+    private Animator _animator;
+    private readonly int _expandCorrectAnimation = Animator.StringToHash("ExpandCorrect");
+    private readonly int _expandIncorrectAnimation = Animator.StringToHash("ExpandIncorrect");
 
-	public static event Action<GameObject, float> s_OnTrashedEvent; // The event that connects to the TrashManager
-	public static event Action<float, SOTrashData> s_OnTrashedEvent2;
-	public static event Action<string, string, float> s_OnLogEvent;
+    // Audio
+    private AudioSource _audioSource;
 
-	private AudioSource _audioSource;
+    private void OnTriggerEnter(Collider target)
+    {
+        if (_binData == null)
+        {
+            Debug.LogError($"Data for the TrashBin has not been assigned");
+            return;
+        }
+        
+        target.TryGetComponent<ITrashable>(out ITrashable trash);  // Check if the GameObject entering the Trigger has an ITrashable
+        float? points = trash.Trashing(_binData._AllowedType);     // and calling the Trashing method which returns a float
+        
+        if (points != null)
+        {
+            s_OnTrashedEvent?.Invoke(gameObject, (float)points); // Casts the points as a float and invokes the OnTrashedEvent
+            s_OnTrashedEvent2?.Invoke((float)points, trash.TrashData());
+            s_OnLogEvent.Invoke(target.gameObject.name, gameObject.name, (float)points);
 
-	// Animations
-	private Animator _animator;
-	private readonly int _expandCorrectAnimation = Animator.StringToHash("ExpandCorrect");
-	private readonly int _expandIncorrectAnimation = Animator.StringToHash("ExpandIncorrect");
+            EnablePolish(points);
+        }
 
+        AudioClip clip = trash.TrashingSound();
+        if (clip != null)
+        {
+            PlayTrashSound(clip);
+        }
+    }
 
+    private void PlayTrashSound(AudioClip clip)
+    {
+        _audioSource.clip = clip;
+        _audioSource.Play();
+    }
 
-	private void OnTriggerEnter(Collider target)
-	{
-		if (_binData == null)
-		{
-			Debug.LogError($"Data for the TrashBin has not been assigned");
-			return;
-		}
+    private void EnablePolish(float? points) // Checks if the value is positive or negative
+    {
+        if (_animator == null)
+        {
+            Debug.LogError($"Animator has not been assigned for this object: {gameObject}");
+            return;
+        }
 
-		target.TryGetComponent<ITrashable>(out ITrashable trash);
+        _animator.Play(points >= 0 ? _expandCorrectAnimation : _expandIncorrectAnimation); // Play corresponding animation
+    }
 
-		float? points = trash.Trashing(_binData._AllowedType); // Check if the GameObject entering the Trigger has an ITrashable
-																// and calling the Trashing method, the method returns a float
-		if (points != null)
-		{
-			s_OnTrashedEvent?.Invoke(gameObject, (float)points); // Casts the points as a float and invokes the OnTrashedEvent
-			s_OnTrashedEvent2?.Invoke((float)points, trash.TrashData());
-			s_OnLogEvent.Invoke(target.gameObject.name, gameObject.name, (float)points);
-
-			EnablePolish(points);
-		}
-
-		AudioClip clip = trash.TrashingSound();
-		if (clip != null)
-		{
-			PlayTrashSound(clip);
-		}
-
-	}
-
-	private void PlayTrashSound(AudioClip clip) 
-	{
-		_audioSource.clip = clip;
-		_audioSource.Play();
-	}
-
-	private void EnablePolish(float? points) // Checks if the value is positive or negative
-	{
-		if (_animator == null)
-		{
-			Debug.LogError($"Animator has not been assigned for this object: {gameObject}");
-			return;
-		}
-		_animator.Play(points >= 0 ? _expandCorrectAnimation : _expandIncorrectAnimation); // Play corresponding animation
-	}
-
-	public void Log() 
-	{
-		
-	}
+    public void Log() { }
 
 
-	#region UnityMethods
+#region UnityMethods
+    private void Awake()
+    {
+        gameObject.tag = "TrashBin";
+    }
 
-	private void Awake() 
-	{
-		gameObject.tag = "TrashBin";
-	}
+    private void Start()
+    {
+        Reset();
+    }
 
-	private void Start()
-	{
-		Reset();
-	}
-
-	// Makes sure that the BoxCollider is a set to a Trigger
-	public void Reset()
-	{
-		_animator = GetComponent<Animator>();
-		_audioSource = GetComponent<AudioSource>();
-		_audioSource.playOnAwake = false;
-		_audioSource.spatialBlend = 1f;
-		GetComponent<BoxCollider>().isTrigger = true;
-		Rigidbody rb = GetComponent<Rigidbody>();
-		rb.isKinematic = true;
-		rb.detectCollisions = true;
-	}
-
-    #endregion
+    // Makes sure that the BoxCollider is a set to a Trigger
+    public void Reset()
+    {
+        _animator                             = GetComponent<Animator>();
+        _audioSource                          = GetComponent<AudioSource>();
+        _audioSource.playOnAwake              = false;
+        _audioSource.spatialBlend             = 1f;
+        GetComponent<BoxCollider>().isTrigger = true;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.isKinematic      = true;
+        rb.detectCollisions = true;
+    }
+#endregion
 }
