5f0a4e42c7d749d2a691211a85975856718e2134
JohanPedersen11 <Johan.munch.2002@gmail.com>
2025-04-07 22:24:19 +0200

    trash can now be spawned to prevent soft lock in tutorial

--- Assets/_IdealabAssets/_Scripts/Tutorial/TutorialTasks/SOTutorialObjectiveOne.cs
+++ Assets/_IdealabAssets/_Scripts/Tutorial/TutorialTasks/SOTutorialObjectiveOne.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Tutorial/Objective/One")]
 public class SOTutorialObjectiveOne : SOTutorialObjectiveBase
 {
-    [SerializeField] private List<GameObject> SO_trashToInstantiate;
+    [SerializeField] private List<GameObject> SO_trashToInstantiate; // Original list
+    private List<GameObject> SO_SpawnedTrashObjects = new(); // Clone of original. To track if any objects are missing. To prvent soft lock
     public override void EnterState(TutorialManager manager) // Start objective
-    {     
+    {
         PlayerVR.S_OnGripStateChanged += SetGripState;
+        PlayerVR.S_TestTrashing += CheckAndRestoreTrash;
+        SO_SpawnedTrashObjects.Clear();
         CreateObjects();
         base.EnterState(manager);
     }
@@ -24,13 +27,36 @@ public class SOTutorialObjectiveOne : SOTutorialObjectiveBase
         Vector3 spawnPosition = TutorialManager.S_Instance.GetSpawnPosition();
         foreach (GameObject prefabObject in SO_trashToInstantiate)
         {
-            Instantiate(prefabObject, spawnPosition, Quaternion.identity);
-        }       
+            GameObject spawned = Instantiate(prefabObject, spawnPosition, Quaternion.identity);
+            SO_SpawnedTrashObjects.Add(spawned);
+        }
+    }
+
+    private void CheckAndRestoreTrash(bool notUsedBool)
+    {
+        bool allGone = true;
+        Debug.Log(SO_SpawnedTrashObjects.Count);
+        foreach (GameObject prefabObject in SO_SpawnedTrashObjects)
+        {
+            if (prefabObject != null)
+            {
+                allGone = false;
+                break;
+            }
+        }
+
+        if (allGone)
+        {
+            Debug.Log("spawn");
+            SO_SpawnedTrashObjects.Clear(); // Clear old references
+            CreateObjects();
+        }
     }
 
     public override void ExitState(TutorialManager manager) // Finish objective
     {
         PlayerVR.S_OnGripStateChanged -= SetGripState;
+        PlayerVR.S_TestTrashing -= CheckAndRestoreTrash;
         base.ExitState(manager);
         
     }
