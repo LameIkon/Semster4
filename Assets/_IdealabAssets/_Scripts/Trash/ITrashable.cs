6b6be4aa2ac361c06bc272dec0b74ac537fe2b22
chaplinchap <jonas.linnemann@live.dk>
2025-04-09 15:50:18 +0200

    Initial work on vomit functionality

--- Assets/_IdealabAssets/_Scripts/Trash/ITrashable.cs
+++ Assets/_IdealabAssets/_Scripts/Trash/ITrashable.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public interface ITrashable
 {
     float Trashing(SortingCategory type);
+    bool Vomit(SortingCategory type);
     SOTrashData TrashData();
     AudioClip TrashingSound();
     void PickUpSound();
