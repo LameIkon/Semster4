2934a21f6dc05c76898e5460d9b24fe534340eea
LameIkon <troelsjelsbak@hotmail.com>
2025-04-10 15:54:38 +0200

    Commit again

--- Assets/_IdealabAssets/_Scripts/FloorTrigger.cs
+++ Assets/_IdealabAssets/_Scripts/FloorTrigger.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
 public class FloorTrigger : MonoBehaviour
 {
    void OnTriggerEnter(Collider target)
